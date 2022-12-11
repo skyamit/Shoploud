c6c292149be7cd184c7af911fc1a075439b6dce7
Amit Kumar <amit7646014@gmail.com>
2022-12-11 08:42:23 +0530

    fixed queries

--- src/main/java/com/example/Shoploud/dao/TproductsDao.java
+++ src/main/java/com/example/Shoploud/dao/TproductsDao.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
 
 import com.example.Shoploud.models.Tproducts;
 
+@Service
 public interface TproductsDao extends JpaRepository<Tproducts,Long>{
     
     // get Categories 
@@ -18,7 +20,7 @@ public interface TproductsDao extends JpaRepository<Tproducts,Long>{
     public List<Tproducts> getProducts(Long fadminId);
 
     @Modifying
-    @Query(name="update products set fname = :fname, fdescription = :fdescription, fcategory = :fcategory, favailableCount = :favailableCount, fselling_price :fsellingPrice, foriginal_price = :foriginalPrice, fupdated_at = :fupdatedAt, fpercentage_off := fpercentageOff where fid = :fid", nativeQuery = true)
+    @Query(name="update products set fname = :fname, fdescription = :fdescription, fcategory = :fcategory, favailable_count = :favailableCount, fselling_price = :fsellingPrice, foriginal_price = :foriginalPrice, fupdated_at = :fupdatedAt, fpercentage_off = :fpercentageOff where fid = :fid", nativeQuery = true)
     public void updateProduct(String fname, String fcategory, String fdescription, Long favailableCount,
     Integer fpercentageOff, Integer foriginalPrice, Integer fsellingPrice,
     String fupdatedAt);
